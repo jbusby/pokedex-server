6bf851e07a8d41122c5da2b765ed68753e163520
Jeff Busby <jbusby@freshbooks.comj>
2018-05-10 15:31:15 -0400

    Added initial models and db structure

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20180510191553) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.integer "identifier"
+    t.integer "generation_id"
+    t.boolean "is_main_series"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pokemon_abilities", force: :cascade do |t|
+    t.integer "pokemon_id"
+    t.integer "ability_id"
+    t.boolean "is_hidden"
+    t.integer "slot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "identifier"
+    t.integer "species_id"
+    t.integer "height"
+    t.integer "weight"
+    t.integer "base_experience"
+    t.integer "order"
+    t.boolean "is_default"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
